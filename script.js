caa47a16e6ccb641e07dfa926f1b0e068ef5fc5e
ROHAN VERMA <145982889+rohanverma445@users.noreply.github.com>
2024-10-22 22:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,12 +36,17 @@ $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Learner", "Developer" , "Freelancer"],
-        typeSpeed: 70,
+        typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-  
+    var typed = new Typed(".typing-2", {
+        strings: ["Learner", "Developer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
@@ -85,115 +90,4 @@ $('.toc-list li a, .navbar .menu li a').click(function(e){
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 500);
-});
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // Sticky navbar on scroll
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-
-        // Scroll-up button show/hide
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    // Slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // Toggle menu/navbar
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-  
-
-
-    // Owl carousel script for extracurricular activities
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-
-    // Smooth scroll for the new sections
-    $('.toc-list li a, .navbar .menu li a').click(function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-    });
-});
-// Owl carousel script for extracurricular activities
-$('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    responsive: {
-        0:{
-            items: 1,
-            nav: false
-        },
-        600:{
-            items: 2,
-            nav: false
-        },
-        1000:{
-            items: 3,
-            nav: false
-        }
-    }
-});
-// Owl carousel script for Educational Background, Skills, and Work Samples
-$('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    responsive: {
-        0:{
-            items: 1,
-            nav: false
-        },
-        600:{
-            items: 2,
-            nav: false
-        },
-        1000:{
-            items: 3,
-            nav: false
-        }
-    }
 });
